220b20c0633a8eaa01844528fa8e6fa9d845e4fd
tankTopTaro <balmoresk3v1n@gmail.com>
2024-10-07 13:40:11 +0800

    add todo list

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,14 @@ const About = () => {
     <section id="about" className='border w-full max-w-[1400px] h-[80vh] flex flex-col items-center justify-center'>
         <h1 className="text-2xl">About Section</h1>
         <UnderConstruction />
+        <p>/ ToDo :</p>
+        <ul>
+          <li>Add Content to About Section.</li>
+          <li>Add Content to Footer</li>
+          <li>Add Scroll Triggered Animations</li>
+          <li>Improve Colors</li>
+          <li>Improve overall design</li>
+        </ul>
     </section>
   )
 }
